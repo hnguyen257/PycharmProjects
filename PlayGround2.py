8811eb4dc758979534de7460fa06f62b50c3ba31
hnguyen257 <hnguyen257@users.noreply.github.com>
2019-07-24 18:34:53 -0500

    done more, except option to skip sorting line

--- PlayGround2.py
+++ PlayGround2.py
@@ -1,26 +1,20 @@
 
-import os
+import sys
+import traceback
+e = open("test_error.txt", 'w+')
+try:
+    a = 1/0
+except:
+    #print traceback.print_exc()
+    #a = str(traceback.print_exc())
+    pass
+a = traceback.format_exc()
+#print a
+'''
+e.close()
+e = open("test_error.txt", 'r').read()
 
-
-f = open('RedoGUI.txt', 'w+')
-
-current_directory = os.getcwd()
-current_file_path = current_directory + '\\' + 'RedoGUI.txt'
-cmd_file_path = current_directory + '\\' + 'RedoGUI.cmd'
-command = "pyinstaller --onefile --noconsole " + current_directory + "\GUI.py"
-
-f.write(command)
-f.close()
-
-print command
-os.rename(current_file_path, cmd_file_path)
-
-os.system(cmd_file_path)
-
-os.rename(current_directory + '\\dist\\GUI.exe', current_directory + '\GUI.exe')
-os.remove(current_directory + '\\GUI.py')
-os.remove(current_directory + '\\GUI.spec')
-os.remove(current_directory + '\\RedoGUI.cmd')
-os.remove(current_directory + '\\redoGUI.py')
-
-
+print "____________"
+print e
+print "____________"
+'''
