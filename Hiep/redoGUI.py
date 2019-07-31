5866c5611d1bea49adfb59ec258e079d96df68ba
hnguyen257 <hnguyen257@users.noreply.github.com>
2019-07-31 10:59:51 -0500

    version 2.0 done, updated redo, not tested yet

---
+++ Hiep/redoGUI.py
@@ -0,0 +1,33 @@
+
+import os
+import shutil
+
+f = open('RedoGUI.txt', 'w+')
+
+current_directory = os.getcwd()
+current_file_path = current_directory + '\\' + 'RedoGUI.txt'
+cmd_file_path = current_directory + '\\' + 'RedoGUI.cmd'
+command = "pip install chardet" + "\n"
+command = command + "pip install pyinstaller" + "\n"
+command = command + "pyinstaller --onefile --noconsole " + current_directory + "\GUI.py"
+f.write(command)
+f.close()
+
+if os.path.exists(cmd_file_path):
+    os.remove(cmd_file_path)
+
+os.rename(current_file_path, cmd_file_path)
+
+os.system(cmd_file_path)
+if os.path.exists(current_directory + '\GUI.exe'):
+    os.remove(current_directory + '\GUI.exe')
+
+os.rename(current_directory + '\\dist\\GUI.exe', current_directory + '\GUI.exe')
+os.remove(current_directory + '\\GUI.py')
+os.remove(current_directory + '\\GUI.spec')
+os.remove(current_directory + '\\RedoGUI.cmd')
+shutil.rmtree(current_directory + '\\build')
+shutil.rmtree(current_directory + '\\dist')
+os.remove(current_directory + '\\redoGUI.py')
+
+
